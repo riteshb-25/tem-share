a9d65828b1bf490da93d6e8b9e68ccbe03c9914f
riteshb-25 <60614754+riteshb-25@users.noreply.github.com>
2023-11-13 03:48:22 +0530

    Update logging.py

--- logging.py
+++ logging.py
@@ -37,3 +37,47 @@ if __name__ == "__main__":
     except Exception as e:
         # Log any exceptions that occur during cancellation
         logger.error(f"Error during Azure subscription cancellation: {str(e)}", exc_info=True)
+
+
+
+import logging
+
+def setup_logger(log_file_path='azure_subscription_cancellation.log'):
+    # Create a logger
+    logger = logging.getLogger('azure_subscription_cancellation')
+    logger.setLevel(logging.DEBUG)
+
+    # Create a file handler and set the level to debug
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    # Create a console handler and set the level to debug
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    # Create a formatter and attach it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+if __name__ == "__main__":
+    # Set up the logger
+    logger = setup_logger()
+
+    # Example usage of the logger
+    try:
+        # Your cancellation logic here
+
+        # Log successful cancellation
+        logger.info("Azure subscription cancellation successful.")
+    except Exception as e:
+        # Log any exceptions that occur during cancellation
+        logger.error(f"Error during Azure subscription cancellation: {str(e)}", exc_info=True)
+
+
