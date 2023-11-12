fbfca54bc48da0b124e9f54bb0b0960e83d216db
riteshb-25 <60614754+riteshb-25@users.noreply.github.com>
2023-11-13 04:00:49 +0530

    Update logging.py

--- logging.py
+++ logging.py
@@ -120,3 +120,113 @@ logger.log_entry("12345678", "Azure Subscription 1", "CancelSubscription", "spn1
 
 # Log entry for a failed task
 logger.log_entry("87654321", "Azure Subscription 2", "CancelSubscription", "spn456", "SPN 2", False)
+
+
+
+/* Note: This code uses the tabulate library for formatting the table. Make sure to install it using pip install tabulate before running the code. You can customize the log_subscription_cancel method and the example usage according to your specific needs.*/
+
+import json
+from tabulate import tabulate  # Make sure to install tabulate using: pip install tabulate
+
+class SubscriptionLogger:
+    def __init__(self):
+        self.logs = []
+
+    def log_subscription_cancel(self, subscription_id, subscription_name, request_name, request_time, status, spn_id, spn_name, task):
+        log_entry = {
+            "subscription_id": subscription_id,
+            "subscription_name": subscription_name,
+            "request_name": request_name,
+            "request_time": request_time,
+            "status": status,
+            "spn": {"spn_id": spn_id, "spn_name": spn_name},
+            "task": task
+        }
+
+        self.logs.append(log_entry)
+
+    def get_logs_table(self):
+        headers = ["Subscription ID", "Subscription Name", "Request Name", "Request Time", "Status", "SPN ID", "SPN Name", "Task"]
+        rows = []
+
+        for log in self.logs:
+            row = [
+                log["subscription_id"],
+                log["subscription_name"],
+                log["request_name"],
+                log["request_time"],
+                log["status"],
+                log["spn"]["spn_id"],
+                log["spn"]["spn_name"],
+                json.dumps(log["task"])
+            ]
+            rows.append(row)
+
+        table = tabulate(rows, headers=headers, tablefmt="pretty")
+        return table
+
+
+# Example Usage:
+logger = SubscriptionLogger()
+logger.log_subscription_cancel(
+    subscription_id="123456",
+    subscription_name="Sample Subscription",
+    request_name="CancelSubscription",
+    request_time="2023-11-13T12:00:00",
+    status=True,
+    spn_id="789",
+    spn_name="ServicePrincipal",
+    task={"cancel_reason": "Project completion"}
+)
+
+# You can log more entries as needed
+
+# Get the logs table
+logs_table = logger.get_logs_table()
+print(logs_table)
+
+
+/*This example assumes that the log data is stored in a CSV file (log_table.csv). You can customize the file path and column names according to your needs. Also, make sure to install the pandas library if you haven't already:*/
+
+import pandas as pd
+from datetime import datetime
+
+class Logger:
+    def __init__(self, log_file_path):
+        self.log_file_path = log_file_path
+        self.columns = ["subscription_id", "subscription_name", "Request_name", "Request_time", "status",
+                        "spn_id", "spn_name", "task"]
+
+    def log_data(self, data):
+        # Create or load the log file as a DataFrame
+        try:
+            log_df = pd.read_csv(self.log_file_path)
+        except FileNotFoundError:
+            log_df = pd.DataFrame(columns=self.columns)
+
+        # Add a timestamp to the data
+        data["Request_time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # Append the new data to the DataFrame
+        log_df = log_df.append(data, ignore_index=True)
+
+        # Save the DataFrame back to the log file
+        log_df.to_csv(self.log_file_path, index=False)
+
+if __name__ == "__main__":
+    # Example of how to use the logger
+    logger = Logger("log_table.csv")
+
+    log_data = {
+        "subscription_id": "123",
+        "subscription_name": "Example Subscription",
+        "Request_name": "Example Request",
+        "status": True,
+        "spn_id": "456",
+        "spn_name": "Example SPN",
+        "task": {"task_id": 789, "task_name": "Example Task"}
+    }
+
+    logger.log_data(log_data)
+
+
